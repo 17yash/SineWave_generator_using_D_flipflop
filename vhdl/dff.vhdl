486c670dc128798074aa465848835088bf1430a9
17yash <36126243+17yash@users.noreply.github.com>
2021-03-17 17:27:39 +0530

    vhdl code & project

---
+++ vhdl/dff.vhdl
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity dff is
+  Port(
+	d : in std_logic;
+	reset : in std_logic;
+	clk : in std_logic;
+	q : out std_logic);
+  end dff;
+
+architecture behave of dff is
+  begin
+  process (d,reset,clk)
+  begin
+    if reset = '1' then q <='0';
+    elsif clk'event and clk = '1' then q <= d;
+    end if;
+    end process;	
+  end behave;
